9a0a20c0f339ee1cd00d621e19a8b3a91ea73772
parkjan4 <32842598+parkjan4@users.noreply.github.com>
2017-11-25 20:47:46 -0500

    Update and rename main.py to Main.py

--- src/models/Main.py
+++ src/models/Main.py
@@ -1,5 +1,3 @@
-import matplotlib
-matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
 from time import clock
@@ -10,8 +8,8 @@ from GBM import GBM
 tickers = ['SPY','IWM','VEU','CSJ','BLV']
 mgmt_fees = [0.000945,0.002,0.0011,0.002,0.0007]     # annual management fees
 
-Y = 4                       # Original number of years (CONSTANT) (WEBAPP INPUT)
-T = 4                       # Number of years left (WEBAPP INPUT)
+Y = 3                       # Original number of years (CONSTANT) (WEBAPP INPUT)
+T = 3                       # Number of years left (WEBAPP INPUT)
 time_step = 1/4             # Trading frequency in years e.g. 1 means annual trading frequency
 N = int(T / time_step)      # Total number of trading periods (WEBAPP INPUT)
 
@@ -29,7 +27,7 @@ for m in range(0,len(mgmt_fees)):
 eff_fees[m+1] = 0           # assume mgmt fees of cash investment is 0
                                             
 # Prices and returns of assets; nrows x nassets
-prices, returns, bt_prices, bt_returns = ia(tickers,time_step)     
+prices, returns = ia(Y,T,tickers,time_step)     
 means = np.mean(returns.T,axis=1)                    # mean return vector; nassets x 1
 cov_mat = np.cov(returns.T)                          # covariance matrix; nassets x nassets
 
@@ -41,13 +39,13 @@ S03 = prices[-1,3]  # Initial stock price for asset 4
 S04 = prices[-1,4]  # Initial stock price for asset 5
 
 ntrials = 10
-start_time = clock()
+# start_time = clock()
 Sprices0 = GBM(ntrials,N,time_step,means,cov_mat,S00,0)
 Sprices1 = GBM(ntrials,N,time_step,means,cov_mat,S01,1)
 Sprices2 = GBM(ntrials,N,time_step,means,cov_mat,S02,2)
 Sprices3 = GBM(ntrials,N,time_step,means,cov_mat,S03,3)
 Sprices4 = GBM(ntrials,N,time_step,means,cov_mat,S04,4)
-print("--- %s seconds ---" % (clock() - start_time))
+# print("--- %s seconds ---" % (clock() - start_time))
 
 ## Example plot of simulated asset prices
 #plt.plot(Sprices0.T,alpha=0.5)
@@ -94,32 +92,54 @@ for i in range(0,ntrials):
         Returns[j+i*nassets,:] = temp[i+j*ntrials,:]
 
 ### Solve the problem!
-init_alloc = [0.5,0.5,0.5,0.5,0.5,0.5] # Recommended initial alloc (WEBAPP INPUT)
-lamb = 0.01                            # User input risk aversion factor
+lamb = 0.016                           # User input risk aversion factor
+                                       # Recommend using: 0, 0.008, 0.012, 0.016, 0.02
 dis_inc = 500                          # comfortable disposable income given the trading period
-init_con = 7000                        # intial contribution to goal
-goal = 10000                           # financial goal (target)
-start_time = clock()
+if Y - T == 0:
+    init_con = 7000                    # intial contribution to goal (User input)
+    init_alloc = [0.15,0.15,0.1,0.05,0.05,0.5] # Recommended initial alloc (WEBAPP INPUT)
+#else:
+#    # the variable shares is a result from last optimization
+#    net_val = np.matrix(np.zeros((len(shares),1)))
+#    for i in range(0,len(shares)-1):
+#        net_val[i] = shares[i] * prices[-1,i]
+#    net_val[i+1] = shares[i+1]              # This is the 'true' net wealth
+#    init_con = np.sum(net_val)
+#    init_alloc = list(net_val / init_con)   # New allocation restriction at "t = 0"
+
+# Financial goal (target) accounted for inflation (constant 2%)
+goal = 10000 * (1 + int_rate_convert(0.02,time_step))**(Y-T) 
+
+# Optimize!
+# start_time = clock()   
 opt_soln, P, q = sp(nassets, ntrials, Y, N, lamb, dis_inc, Returns, init_con, goal, eff_fees, init_alloc)
-print("--- %s seconds ---" % (clock() - start_time))     # Measure run time
+# print("--- %s seconds ---" % (clock() - start_time))     # Measure run time
 
-# Display major results (mean, variance, ...)
 dv = np.matrix(opt_soln['x'])
-print(dv[dv.shape[0]-nassets*ntrials:dv.shape[0]]) 
-print('Mean wealth across all scenarios is $',round(float(q.T * dv),2))
-print('Variance among wealths across all scenarios is',round(float(dv.T * P * dv),2))
+mean_term_wealth = round(float(q.T * dv),2)         # Mean of terminal wealths
+mean_var_wealth = round(float(dv.T * P * dv),2)     # Variance of wealths
 
-# Pie Chart: Terminal average asset allocation across all scenarios
-temp = dv[dv.shape[0]-nassets*ntrials:dv.shape[0]]
-term_wealths = np.zeros((nassets,ntrials))
+# t = 1 average asset allocation across all scenarios (need this for next optimization)
+collect = np.matrix(np.zeros((nassets,ntrials)))
+ctr = 0
 for s in range(0,ntrials):
-    for a in range(0,nassets):
-        term_wealths[a,s] = temp[a + s*nassets]
-taaa = np.mean(term_wealths,axis=1)
-plt.pie(taaa,labels=['SPY','IWM','VEU','CSJ','BLV','Cash Investment'],autopct='%1.1f%%')
+    ctr = ctr + (nassets + 1)
+    collect[:,s] = dv[ctr:ctr + nassets]
+avg_alloc = np.mean(collect,axis=1) 
+shares = avg_alloc
+shares[0:-1] = avg_alloc[0:-1] / prices[-1,:].T # Cash investment is left in units of $
+
+## Pie Chart: Terminal average asset allocation across all scenarios
+#temp = dv[dv.shape[0]-nassets*ntrials:dv.shape[0]]
+#term_wealths = np.zeros((nassets,ntrials))
+#for s in range(0,ntrials):
+#    for a in range(0,nassets):
+#        term_wealths[a,s] = temp[a + s*nassets]
+#taaa = np.mean(term_wealths,axis=1)
+#plt.pie(taaa,labels=['SPY','IWM','VEU','CSJ','BLV','Cash Investment'],autopct='%1.1f%%')
 
 #### Optimize for different values of lamb and plot changes in mean and variance
-#lamb = np.linspace(0,1,11)
+#lamb = np.linspace(0,0.1,11)
 #mean_wealths = []
 #var_wealths = []
 #for l in lamb[0:len(lamb)-1]:
