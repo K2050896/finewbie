a1c2779e7364ad0c80bb2339c8d008378a8a38b9
K2050896 <kl.zhang@mail.utoronto.ca>
2017-11-26 17:51:46 -0500

    Update and rename setting.py to profile.py

--- src/models/profile.py
+++ src/models/profile.py
@@ -1,92 +1,29 @@
 from src.common.database import Database
 
-class Setting(object):
-    def __init__(self, id, port_id, name, Y,T , dis_inc, init_con, goal, t1, t2, r1, r2, r3, r4):
+class Profile(object):
+    def __init__(self, id, port_id, name, Y, T , dis_inc, init_con, goal, importance, r1, r2, r3, r4):
         #T1 is the time horizon 
         self.port_id = port_id #portfolio id
         self.name = name
         self.Y = Y
         self.T = T
-        if length <= 1:
-            t = 1
-        if length > 1 and <=2:
-            t = 2
-        elif length > 2 and length <= 5:
-            t = 3
-        elif length > 5 and length <= 10:
-            t = 4
-        elif length > 10:
-            t = 5
-        
-        (6 - importance + t)/2 = time
-        r = r1+ r2+ r3+ r4
-        
-        self.lamb
-        self.init_alloc = 
-        
-        
-        
         self.dis_inc = dis_inc
         self.init_con = init_con
         self.goal = goal
-
-
-    def json(self):
-        return {
-            "port_id": self.port_id,
-            "name": self.name,
-            "length remaining": self.T,
-            "lamb": self.lamb,
-            "dis_inc": self.dis_inc,
-            "init_con": self.init_con,
-            "init_alloc": self.init_alloc
-            "goal": self.goal
-            
-        }
-
-    def save_to_mongo(self):
-        Database.insert("settings", self.json())
-
-   
-    @staticmethod
-    def from_mongo(port_id):
-        data = Database.find_one(collection='settings', query={'port_id': port_id})
-        if data is not None:
-            return data
-
-    @staticmethod
-    def delete_setting(port_id):
-        Database.delete_all(collection='settings', query ={"port_id": port_id})
-    
-    @staticmethod
-    def convert_setting(settings):
-        name = setting[0] # string
-        length = setting[1] # float
-        budget = setting[2] # int
-        importance = setting[3] # 1-5
-        initial_contribution = setting[5] #int
-        value_assets = setting[6] #int
-        value_liabilities = setting[7] #int
-        risk1 = setting[8] # 1-4
-        risk2 = setting[9] # 1-4
-        risk3 = setting[10] # 1-4
-        risk4 = setting[11] # 1-4
-        risk5 = setting[12] # 1-4
         
-        risk = (risk1 + risk2 + risk3 + risk4 + risk5) * 2 
-        
-        if length <= 1:
+        if Y <= 1:
             t = 1
-        if length > 1 and <=2:
+        if Y > 1 and Y <=2:
             t = 2
-        elif length > 2 and length <= 5:
+        elif Y > 2 and Y <= 5:
             t = 3
-        elif length > 5 and length <= 10:
+        elif Y > 5 and Y <= 10:
             t = 4
-        elif length > 10:
+        elif Y > 10:
             t = 5
         
-        (6 - importance + t)/2 = time
+        time = (6 - importance + t)/2 
+        risk = (r1+ r2+ r3+ r4)*2
         
         if time > 0 and time <= 1:
             if risk > 0 and risk <= 18:
@@ -141,7 +78,51 @@ class Setting(object):
                 profile = "Moderately Aggressive"
             elif risk > 31 and risk <= 40:
                 profile = "Aggressive"
-        return name, profile
         
-        
-        
+        if profile == "Conservative":
+            self.init_alloc = [0.15,0,0.05,0.25,0.25,0.3]
+            self.lamb = 0.00
+        elif profile == "Moderately Conservative":
+            self.init_alloc = [0.25,0.05,0.10,0.25,0.25,0.10]
+            self.lamb = 0.25
+        elif profile == "Moderate":
+            self.init_alloc = [0.35,0.10,0.15,0.175,0.175,0.05]
+            self.lamb = 0.50
+        elif profile == "Moderately Aggressive":
+            self.init_alloc = [0.45,0.15,0.20,0.075,0.075,0.05]
+            self.lamb = 0.75
+        elif profile == "Aggressive":
+            self.init_alloc = [0.50,0.20,0.25,0,0,0.05]
+            self.lamb = 1.00
+            
+
+
+
+    def json(self):
+        return {
+            "port_id": self.port_id,
+            "name": self.name,
+            "lenght of goal": self.Y
+            "length remaining": self.T,
+            "lamb": self.lamb,
+            "dis_inc": self.dis_inc,
+            "init_con": self.init_con,
+            "init_alloc": self.init_alloc,
+            "goal": self.goal
+            
+        }
+
+    def save_to_mongo(self):
+        Database.insert("profiles", self.json())
+
+   
+    @staticmethod
+    def from_mongo(port_id):
+        data = Database.find_one(collection='profiles', query={'port_id': port_id})
+        if data is not None:
+            return data
+
+    @staticmethod
+    def delete_setting(port_id):
+        Database.delete_all(collection='profiles', query ={"port_id": port_id})
+   
