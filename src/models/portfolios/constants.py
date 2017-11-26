a72f10fcd4443e0d19f836c9c76499f378302e1e
12sowmya <sowmya027@gmail.com>
2017-11-26 18:45:55 -0500

    more files

--- src/models/portfolios/constants.py
+++ src/models/portfolios/constants.py
@@ -4,3 +4,11 @@ INT_RATE = 0.01
 COUPONS = [0.016, 0.045]
 INFLATION = 0.02
 TRANS_COSTS = [0, 0, 0, 0, 0]
+
+RISK_LABELS = ['Conservative', 'Moderately Conservative', 'Moderate', 'Moderately Aggressive', 'Aggressive']
+RISK_INDECES = [0.00, 0.25, 0.50, 0.75, 1.00]
+INITIAL_ALLOC = [[0.15,0,0.05,0.25,0.25,0.3],
+                 [0.25,0.05,0.10,0.25,0.25,0.10],
+                 [0.35,0.10,0.15,0.175,0.175,0.05],
+                 [0.45,0.15,0.20,0.075,0.075,0.05],
+                 [0.50,0.20,0.25,0,0,0.05]]
