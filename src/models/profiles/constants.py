c186a41bf90049bae14c7cdb58e07c8b28568202
K2050896 <kl.zhang@mail.utoronto.ca>
2017-11-26 19:58:52 -0500

    Update constants.py

--- src/models/profiles/constants.py
+++ src/models/profiles/constants.py
@@ -1,5 +1,5 @@
 RISK_LABELS = ['Conservative', 'Moderately Conservative', 'Moderate', 'Moderately Aggressive', 'Aggressive']
-RISK_INDECES = [0.00, 0.25, 0.50, 0.75, 1.00]
+RISK_INDECES = [1.00, 0.75, 0.50, 0.25, 0.00]
 INITIAL_ALLOC = [[0.15,0,0.05,0.25,0.25,0.3],
                  [0.25,0.05,0.10,0.25,0.25,0.10],
                  [0.35,0.10,0.15,0.175,0.175,0.05],
