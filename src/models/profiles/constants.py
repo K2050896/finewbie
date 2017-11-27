f2be2372428c98e013e2cdd59f7b60e4e1d266ff
12sowmya <sowmya027@gmail.com>
2017-11-26 19:38:58 -0500

    Proper file structure hopefully - 7:39pm Sunday

---
+++ src/models/profiles/constants.py
@@ -0,0 +1,7 @@
+RISK_LABELS = ['Conservative', 'Moderately Conservative', 'Moderate', 'Moderately Aggressive', 'Aggressive']
+RISK_INDECES = [0.00, 0.25, 0.50, 0.75, 1.00]
+INITIAL_ALLOC = [[0.15,0,0.05,0.25,0.25,0.3],
+                 [0.25,0.05,0.10,0.25,0.25,0.10],
+                 [0.35,0.10,0.15,0.175,0.175,0.05],
+                 [0.45,0.15,0.20,0.075,0.075,0.05],
+                 [0.50,0.20,0.25,0,0,0.05]]
