686fa64197531385b7f54bbcc0c889dfd69da0cf
K2050896 <kl.zhang@mail.utoronto.ca>
2017-11-26 22:55:24 -0500

    Update database.py

--- src/common/database.py
+++ src/common/database.py
@@ -8,7 +8,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client["fullstack"]
+        Database.DATABASE = client["heroku_g0lnhdnb"]
 
     @staticmethod
     def insert(collection, data):
